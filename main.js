0154512b8366d028d128896ef56d2950e63ea5e0
tim7398 <timqin@Tim-Qins-MacBook-Pro.local>
2015-07-16 21:34:46 -0400

    new site

---
+++ main.js
@@ -0,0 +1,14 @@
+var main function() {
+	
+	$(window).resize(function()
+	{
+		$('#container').css(
+		{
+			position:'absolute',
+   			left: ($(window).width() - $('#container').outerWidth())/2,
+   			top: ($(window).height() - $('#container').outerHeight())/2
+		});
+	});
+}
+
+$(window).resize();
