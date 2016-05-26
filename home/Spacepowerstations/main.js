5676a7125a5fd4fa22b20bc6b9ccf3fa21ffb1d9
Tim Qin <a1@1deMacBook-Pro.local>
2016-05-26 17:04:51 -0400

    small change

--- home/Spacepowerstations/main.js
+++ home/Spacepowerstations/main.js
@@ -1,32 +1,35 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
- var counter = 0;
+
   $('.icon-menu').click(function() 
   {
-    counter = counter +1;
+    
     $('#navigation_container').animate({
       left: "10em"
     }, 200);
     
     $('#nav_bar').animate(
     {left:"-7em"}, 200);
+    $('.close').show();
+
+    $(this).hide();
   
   });
 
   /* Then push them back */
  
-  $('#navigation_container').click(function() {
-    if( counter == 2)
-  {
-    $(this).animate({
+  $('.close').click(function() {
+    
+    $('#navigation_container').animate({
       left: "0em"
     }, 200);
 
+    $(this).hide();
+    $('.icon-menu').show();
     $('#nav_bar').animate({
       left: "-800px"
     }, 200);
-counter = 0;
-}
+
   });
  
 
