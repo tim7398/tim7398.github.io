b1fa84c21ce7d8d9c5f96fd019e37a44322ee8af
Tim Qin <a1@1deMacBook-Pro.local>
2016-05-26 13:18:33 -0400

    javscript

---
+++ home/Spacepowerstations/main.js
@@ -0,0 +1,36 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+ var counter = 0;
+  $('.icon-menu').click(function() 
+  {
+    counter = counter +1;
+    $('#navigation_container').animate({
+      left: "6em"
+    }, 200);
+    
+    $('#nav_bar').animate(
+    {left:"-5em"}, 200);
+  
+  });
+
+  /* Then push them back */
+ 
+  $('#navigation_container').click(function() {
+    if( counter == 2)
+  {
+    $(this).animate({
+      left: "0em"
+    }, 200);
+
+    $('#nav_bar').animate({
+      left: "-800px"
+    }, 200);
+counter = 0;
+}
+  });
+ 
+
+};
+
+
+$(document).ready(main);
